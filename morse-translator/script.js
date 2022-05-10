39a0f69f104722aee91848eb7f2b3a1ffe9f8711
Manuel La Placa <89313692+Mannylapli@users.noreply.github.com>
2022-05-10 15:17:38 +0100

    Add files via upload
    html page layout

---
+++ morse-translator/script.js
@@ -0,0 +1,17 @@
+// Overview
+// You’re tasked with creating a morse code translator using OOP. As an extension it should be
+// able to translate based on user input from the DOM. Consider how you will store the morse
+// characters in JS to make translation easier.
+
+// The classes you use should store all the information needed to translate between Morse and
+// English, including the alphabets themselves, the word to translate and possibly the eventual
+// output as it’s changing.
+
+// Some of the methods might include how to add a word to translate, how to break it down to aid
+// the translation and you’ll have to think carefully about how and when you call these methods to
+// make sure the state of the object is getting changed correctly.
+
+// The logic for translating both ways won’t be exactly the same but some methods may be similar
+// enough that you might be able to create a base class to extend from in order to share logic
+// between the two translators (Morse => English, English => Morse).
+// Remember to make a plan/pseudocode before diving right in.
